6d0a8dd148a48112120e16eb2e81ed9084c381d7
Han <whgml012@naver.com>
2024-09-28 18:41:47 +0900

    1_1

---
+++ Gyeonghun/1_1.py
@@ -0,0 +1,24 @@
+def solution(n, words):
+    turn = 1
+    answer = [words[0]]
+
+    for i in range(1, len(words)):
+        if words[i] in answer:
+            print("a")
+            return [i % n + 1, turn]
+        
+        if words[i-1][-1] != words[i][0]:
+            print("b")
+            return [i % n + 1, turn]
+        
+        
+        answer.append(words[i])
+        print(answer)
+        
+        if (i + 1) % n == 0:
+            turn += 1
+        
+    return [0, 0]
+
+
+print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
